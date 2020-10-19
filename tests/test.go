7aca351376bd21b69b49ddc111fab0c6f80aa102
Vortex <xvscode@gmail.com>
2020-10-19 20:04:59 +0800

    commit message

--- tests/test.go
+++ tests/test.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"net/http"
 	"time"
 )
 
 type Person struct {
 	Name     string    `form:"name" binding:"required"`
 	Address  string    `form:"address" binding:"required"`
-	Birthday time.Time `form:"birthday" binding:"required"`
+	Birthday time.Time `form:"birthday"`
 }
 
 func startPage(c *gin.Context) {
@@ -16,13 +17,15 @@ func startPage(c *gin.Context) {
 	// If `GET`, only `Form` binding engine (`query`) used.
 	// If `POST`, first checks the `content-type` for `JSON` or `XML`, then uses `Form` (`form-data`).
 	// See more at https://github.com/gin-gonic/gin/blob/master/binding/binding.go#L48
-	if c.ShouldBind(&person) != nil {
-		if person.Name == "xueyuanjun" && person.Address == "123456" {
-			c.JSON(200, gin.H{"status": "you are logged in"})
-		} else {
-			c.JSON(401, gin.H{"status": "unauthorized"})
-		}
+	if err := c.ShouldBind(&person); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
 	}
+	if person.Name != "xueyuanjun" || person.Address != "123456" {
+		c.JSON(http.StatusUnauthorized, gin.H{"status": "unauthorized"})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"status": "you are logged in"})
 
 	//c.String(200, "Success")
 }
