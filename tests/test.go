948c6f13060998b08ff85a07a460905439c2ae8e
Vortex <xvscode@gmail.com>
2020-10-19 19:59:13 +0800

    commit message

--- tests/test.go
+++ tests/test.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"log"
 	"time"
 )
 
 type Person struct {
-	Name     string    `form:"name"`
-	Address  string    `form:"address"`
-	Birthday time.Time `form:"birthday"`
+	Name     string    `form:"name" binding:"required"`
+	Address  string    `form:"address" binding:"required"`
+	Birthday time.Time `form:"birthday" binding:"required"`
 }
 
 func startPage(c *gin.Context) {
@@ -18,12 +17,14 @@ func startPage(c *gin.Context) {
 	// If `POST`, first checks the `content-type` for `JSON` or `XML`, then uses `Form` (`form-data`).
 	// See more at https://github.com/gin-gonic/gin/blob/master/binding/binding.go#L48
 	if c.ShouldBind(&person) != nil {
-		log.Println(person.Name)
-		log.Println(person.Address)
-		log.Println(person.Birthday)
+		if person.Name == "xueyuanjun" && person.Address == "123456" {
+			c.JSON(200, gin.H{"status": "you are logged in"})
+		} else {
+			c.JSON(401, gin.H{"status": "unauthorized"})
+		}
 	}
 
-	c.String(200, "Success")
+	//c.String(200, "Success")
 }
 
 func main() {
