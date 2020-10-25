5baa64257a256b840d8901a9b6cc9423ad8e0148
Vortex <xvscode@gmail.com>
2020-10-25 17:12:42 +0800

    commit message

--- app/controllers/FileController.go
+++ app/controllers/FileController.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 var (
@@ -13,5 +14,20 @@ type FileController struct {
 }
 
 func (fileCtr FileController) DownLoadFile(ctx *gin.Context) {
+	response, err := http.Get("https://apk.dalongyun.com/dalongyun_page/v2.0.1/new_moban/ad.jpg")
+	if err != nil || response.StatusCode != http.StatusOK {
+		ctx.Status(http.StatusServiceUnavailable)
+		return
+	}
 
+	reader := response.Body
+	contentLength := response.ContentLength
+	contentType := response.Header.Get("Content-Type")
+
+	extraHeaders := map[string]string{
+		"Content-Disposition": `attachment; filename="gopher.png"`,
+	}
+
+	ctx.DataFromReader(http.StatusOK, contentLength, contentType, reader, extraHeaders)
+	//fileCtr.SuccessResponse(ctx,"")
 }
